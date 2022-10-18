9b33956319fe02e3acdb9bf9f75b3d4685c48aae
chu9741 <chu9741@gmail.com>
2022-10-18 12:59:20 +0900

    init

---
+++ src/main/java/com/example/WithRun/WithRunApplication.java
@@ -0,0 +1,13 @@
+package com.example.WithRun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WithRun/WithRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WithRun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
